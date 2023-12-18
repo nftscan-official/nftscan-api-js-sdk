b6b5b50cae88e281d6662ebe624466888c4a918f
pqy <1156822095@qq.com>
2023-12-18 18:15:09 +0800

    feat: change init func

--- src/util/nftscan.init.ts
+++ src/util/nftscan.init.ts
@@ -1,13 +1,3 @@
 import { initHttpConfig } from '../http/nftscan.http';
 
-export default class NftscanInit {
-  static created = false;
-
-  static onCreate() {
-    if (this.created) {
-      return;
-    }
-    this.created = true;
-    initHttpConfig();
-  }
-}
+initHttpConfig();
